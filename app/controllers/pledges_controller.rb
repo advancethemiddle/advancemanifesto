f5271c15f6f7f8caa09517b9215dfaf23e31631a
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-10-16 01:10:42 -0400

    Add support for creating new pledges * Add Redis configurations * Broadcast pledge counts to view * Add routing, controller actions, and new view

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,23 @@
+class PledgesController < ApplicationController
+  def new
+    @pledge = Pledge.new
+  end
+
+  def create
+    @pledge = Pledge.new(pledge_params)
+
+    if @pledge.save
+      flash[:success] = "Thanks for signing!"
+      redirect_to :root
+    else
+      flash[:error] = "Eek! We had trouble saving your pledge. Please try again later."
+      redirect_to :root
+    end
+  end
+
+  private
+
+  def pledge_params
+    params.require(:pledge).permit(:first_name, :last_name, :email)
+  end
+end
