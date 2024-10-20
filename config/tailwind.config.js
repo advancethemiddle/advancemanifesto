e84f9901fe05a2b6a85bf2ef940471ebdf1f088d
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-10-20 17:33:57 -0400

    Add share section copy and styles [ci skip]

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,13 +9,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
       colors: {
         mango: "#FD6119",
         persimmon: "#EA4A00",
         soil: "#222222",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      maxWidth: {
+        content: "894px",
       },
     },
   },
