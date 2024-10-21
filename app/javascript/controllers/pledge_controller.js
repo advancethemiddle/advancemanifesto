712b68075b7ceee539749ea3126e253f873c8a29
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-10-21 00:07:23 -0400

    Add existential js checks to prevent console errors

--- app/javascript/controllers/pledge_controller.js
+++ app/javascript/controllers/pledge_controller.js
@@ -2,20 +2,24 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['form', 'hero', 'pitch']
-  heroHeight = this.heroTarget.offsetHeight
-  heroBuffer = 300
-  pitchYPadding = 100 * 2
-  pitchHeight = this.pitchTarget.offsetHeight - (this.heroBuffer + this.pitchYPadding)
-  heroPitchHeight = this.heroHeight + this.pitchHeight
-  form = this.formTarget
-  inputs = this.form.querySelectorAll('input')
-  formParagraphs = this.form.querySelectorAll('p')
-  formLists = this.form.querySelectorAll('ul')
-  formArrow = this.form.querySelector('svg')
 
   connect() {
+    this.heroHeight = this.hasHeroTarget ? this.heroTarget.offsetHeight : null
+    this.heroBuffer = 300
+    this.pitchYPadding = 100 * 2
+    this.pitchHeight = this.hasPitchTarget ? this.pitchTarget.offsetHeight - (this.heroBuffer + this.pitchYPadding) : null
+    this.heroPitchHeight = this.heroHeight + this.pitchHeight
+    this.form = this.hasFormTarget ? this.formTarget : null
+    this.inputs = this.hasFormTarget ? this.form.querySelectorAll('input') : null
+    this.formParagraphs = this.hasFormTarget ? this.form.querySelectorAll('p') : null
+    this.formLists = this.hasFormTarget ? this.form.querySelectorAll('ul') : null
+    this.formArrow = this.hasFormTarget ? this.form.querySelector('svg') : null
+
     document.addEventListener('scroll', this.handleDocumentScroll.bind(this))
-    this.inputs.forEach(input => input.addEventListener('focus', this.handleInputFocus.bind(this)))
+
+    if (this.inputs) {
+      this.inputs.forEach(input => input.addEventListener('focus', this.handleInputFocus.bind(this)))
+    }
   }
 
   handleDocumentScroll() {
@@ -42,52 +46,56 @@ export default class extends Controller {
   }
 
   toggleOpacity(element, targetOpacity) {
-    if (targetOpacity === 0) {
-      if (element.classList.contains('opacity-100')) {
-        element.classList.remove('opacity-100')
-        element.classList.add('opacity-0')
+    if (element) {
+      if (targetOpacity === 0) {
+        if (element.classList.contains('opacity-100')) {
+          element.classList.remove('opacity-100')
+          element.classList.add('opacity-0')
+        }
+      } else if (targetOpacity === 100) {
+        if(element.classList.contains('opacity-0')) {
+          element.classList.remove('opacity-0')
+          element.classList.add('opacity-100')
+        }
+      } else {
+        throw new Error('targetOpacity must be a value equal to 0 or 100')
       }
-    } else if (targetOpacity === 100) {
-      if(element.classList.contains('opacity-0')) {
-        element.classList.remove('opacity-0')
-        element.classList.add('opacity-100')
-      }
-    } else {
-      throw new Error('targetOpacity must be a value equal to 0 or 100')
     }
   }
 
   toggleFormState(targetState) {
-    if (targetState === 'relative') {
-      if (this.form.classList.contains('fixed')) {
-        this.form.classList.remove('fixed', 'py-8')
-        this.form.classList.add('relative', 'py-24')
-        this.formParagraphs.forEach(element => {
-          element.classList.add('block', 'opacity-100')
-          element.classList.remove('hidden', 'opacity-0')
-        })
-        this.formLists.forEach(element => {
-          element.classList.add('block', 'opacity-100')
-          element.classList.remove('hidden', 'opacity-0')
-        })
-        this.toggleOpacity(this.formArrow, 100)
+    if (this.form) {
+      if (targetState === 'relative') {
+        if (this.form.classList.contains('fixed')) {
+          this.form.classList.remove('fixed', 'py-8')
+          this.form.classList.add('relative', 'py-24')
+          this.formParagraphs.forEach(element => {
+            element.classList.add('block', 'opacity-100')
+            element.classList.remove('hidden', 'opacity-0')
+          })
+          this.formLists.forEach(element => {
+            element.classList.add('block', 'opacity-100')
+            element.classList.remove('hidden', 'opacity-0')
+          })
+          this.toggleOpacity(this.formArrow, 100)
+        }
+      } else if (targetState === 'fixed') {
+        if (this.form.classList.contains('relative')) {
+          this.form.classList.remove('relative', 'py-24')
+          this.form.classList.add('fixed', 'py-8')
+          this.formParagraphs.forEach(element => {
+            element.classList.add('hidden', 'opacity-0')
+            element.classList.remove('block', 'opacity-100')
+          })
+          this.formLists.forEach(element => {
+            element.classList.add('hidden', 'opacity-0')
+            element.classList.remove('block', 'opacity-100')
+          })
+          this.toggleOpacity(this.formArrow, 0)
+        }
+      } else {
+        throw new Error("targetState must be a value equal to 'relative' or 'fixed'")
       }
-    } else if (targetState === 'fixed') {
-      if (this.form.classList.contains('relative')) {
-        this.form.classList.remove('relative', 'py-24')
-        this.form.classList.add('fixed', 'py-8')
-        this.formParagraphs.forEach(element => {
-          element.classList.add('hidden', 'opacity-0')
-          element.classList.remove('block', 'opacity-100')
-        })
-        this.formLists.forEach(element => {
-          element.classList.add('hidden', 'opacity-0')
-          element.classList.remove('block', 'opacity-100')
-        })
-        this.toggleOpacity(this.formArrow, 0)
-      }
-    } else {
-      throw new Error("targetState must be a value equal to 'relative' or 'fixed'")
     }
   }
 
